01eeff118d52fd818c9b30034ebb2243232f03c8
AntonPazniak <poznyak969@gmail.com>
2025-05-26 11:44:10 +0200

    initial commit

---
+++ src/test/java/com/example/driedfruitstore/DriedFruitStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.driedfruitstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriedFruitStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
